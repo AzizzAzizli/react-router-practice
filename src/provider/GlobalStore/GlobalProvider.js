4fe3e521ed1a80406c6707a159426b72aea6825f
AzizAzizli <azizliaziz025@gmail.com>
2023-11-21 23:58:03 +0400

    fixed some localstorage errors

--- src/provider/GlobalStore/GlobalProvider.js
+++ src/provider/GlobalStore/GlobalProvider.js
@@ -1,46 +1,34 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
+export const globalContext = createContext();
 
-export const globalContext=createContext()
+export const GlobalProvider = ({ children }) => {
+  let localFavs = JSON.parse(localStorage.getItem("Favs")) ?? [];
 
+  const [favs, setFavs] = useState([]);
 
-export const GlobalProvider = ({children}) => {
+  function handleFavs(item) {
+    const inFavs = localFavs.find((i) => i.imdbID === item.imdbID);
+    if (inFavs) {
+      setFavs((prev) => prev.filter((i) => i.imdbID !== item.imdbID));
+    localFavs = localFavs.filter((el) => el.imdbID !== item.imdbID);
+    
+      localStorage.setItem("Favs", JSON.stringify(localFavs));
+    } else {
+      setFavs((prev) => [...prev, item]);
+      localFavs.push(item);
+      localStorage.setItem("Favs", JSON.stringify(localFavs));
+    }
+  }
 
-let localFavs = JSON.parse(localStorage.getItem("Favs")) ?? [];
-
-const [favs,setFavs]=useState([])
-
-
-function handleFavs(item){
-
-const inFavs=favs.find(i=>i.imdbID===item.imdbID)
-if(inFavs){
-
-setFavs(prev=>prev.filter(i=>i.imdbID!==item.imdbID))
-localFavs.filter(el=>el.imdbID!==item.imdbID)
-localStorage.setItem("Favs", JSON.stringify(localFavs));
-
-
-}else{
-    setFavs(prev=>[...prev,item])
-localFavs.push(item)
-localStorage.setItem("Favs", JSON.stringify(localFavs));
-
-
-}
-
-
-}
-
-const values={
+  const values = {
     favs,
     setFavs,
     handleFavs,
-}
-// console.log(favs);
-
+  };
+  // console.log(favs);
 
   return (
     <globalContext.Provider value={values}>{children}</globalContext.Provider>
-  )
-}
+  );
+};
